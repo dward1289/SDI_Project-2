a82f6a63a378cb91fdc99fd5cf16a0254e3b2136
Devona Ward <dward1289@fullsail.edu>
2012-05-09 16:02:37 -0400

    Array Func. added

--- ward_devona_assignment2.js
+++ ward_devona_assignment2.js
@@ -9,7 +9,45 @@ var stageCome = function () {
 	}
 	console.log("Looks like we have some great performers tonight!");
 	};
+
+
+//Boolean function
+var whoUp = function () {
+	var i = 2;
 	
-//Procedure function code here...
+	if (i < rappers.length){
+	return console.log("Looks like " + rappers[i] + " will take the stage first.");
+	}
+	
+	else{
+	return console.log("Looks like " + rappers[1] + " will take the stage first.");
+	}
+	
+	};
+
+//String function
+var exitNow = function () {
+	var leaving = console.log("We enjoyed " + rappers + " for tonght!");
+	return exitNow;
+	};
+
+      
+//Array function
+var seeHim = function(man) {
+            var rapName = rappers[man],
+            minutesStage = stageTime[stageNumber];
+            console.log("Coming to the stage is " + rapName + " for " + minutesStage + " minutes. Enjoy!");
+            for (var minutes = 0; minutes < minutesStage; minutes += 15) {
+                    var minutesRemain = minutesStage - minutes;
+                    console.log( rapName + " has " + minutesRemain + " minutes left. " + minutes + " minutes have past already?");
+            }
+            console.log( rapName + " has completed their performance.");
+        };
+
+        for (var stageNumber = 0; stageNumber < rappers.length; stageNumber++) {
+            seeHim(stageNumber); 
+        };
+
+
 
 
