574ad4af38cd8efdaa870205086f0b5a198e2d6e
Devona Ward <dward1289@fullsail.edu>
2012-05-09 18:34:50 -0400

    Project complete

--- ward_devona_assignment2.js
+++ ward_devona_assignment2.js
@@ -13,14 +13,14 @@ var stageCome = function () {
 
 //Boolean function
 var whoUp = function () {
-	var i = 2;
+	var i = 0;
 	
 	if (i < rappers.length){
-	return console.log("Looks like " + rappers[i] + " will take the stage first.");
+	return console.log(rappers[0] + " will take the stage first.");
 	}
 	
 	else{
-	return console.log("Looks like " + rappers[1] + " will take the stage first.");
+	return console.log(rappers[2] + " will take the stage first.");
 	}
 	
 	};
@@ -31,23 +31,48 @@ var exitNow = function () {
 	return exitNow;
 	};
 
-      
+//Procedure function is executed first.
+stageCome();
+
+//Boolean function is executed second.
+whoUp();     
+
+//Array function is executed third.
 //Array function
-var seeHim = function(man) {
-            var rapName = rappers[man],
-            minutesStage = stageTime[stageNumber];
-            console.log("Coming to the stage is " + rapName + " for " + minutesStage + " minutes. Enjoy!");
+var seeHim = function (man) {
+	 
+            var rapName = rappers[man];
+            var minutesStage = stageTime[stageNumber];
+            console.log("Coming to the stage is " + rapName + " for " + minutesStage + " minutes. Enjoy!");         
             for (var minutes = 0; minutes < minutesStage; minutes += 15) {
                     var minutesRemain = minutesStage - minutes;
                     console.log( rapName + " has " + minutesRemain + " minutes left. " + minutes + " minutes have past already?");
             }
-            console.log( rapName + " has completed their performance.");
+            var doneNow = console.log( rapName + " has completed their performance.");
+         	return doneNow;
         };
 
         for (var stageNumber = 0; stageNumber < rappers.length; stageNumber++) {
             seeHim(stageNumber); 
+            
         };
 
+//Number function
+var money = function () {
+	var rapMen = 3
+	var totalPpl = rapMen * 120000
+	while (rapMen <= 3) {
+	var people = console.log("There were about " + totalPpl + " people at the concert tonight!");
+	return people;
+	}
+	};
+	
+//Number function is executed fourth.
+money();
 
+//String function is executed last.
+exitNow();	
+	
+	
 
 
