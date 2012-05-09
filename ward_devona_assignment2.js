a77a41b658a613569bc4be6a6dcd19dfbc879554
Devona Ward <dward1289@fullsail.edu>
2012-05-09 10:13:54 -0400

    Procedure added

--- ward_devona_assignment2.js
+++ ward_devona_assignment2.js
@@ -1,1 +1,15 @@
-alert("JavaScript works!");
+var rappers = [ "Jay Z","Drake","J Cole" ];
+var stageTime = [ 30, 45, 60 ];
+
+//Procedure function
+var stageCome = function () {
+	for (var rapNum = 0; rapNum < rappers.length; rapNum++){
+	var rapName = rappers[rapNum], time = stageTime[rapNum];
+	console.log("Tonight we will have " + rapName + " for " + time + " minutes!");
+	}
+	console.log("Looks like we have some great performers tonight!");
+	};
+	
+//Procedure function code here...
+
+
